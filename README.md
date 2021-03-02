ebdd5468259bec9b5c9596d34a11c3cb6b7ac4d4
zypeh <zypeh.geek@gmail.com>
2021-03-02 11:19:45 +0800

    adding fnv implementation for benckmark

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # xxhash-hs
 
-> This is a learn-by-doing project.
+**(incomplete)** xxHash3 implementation written in Haskell.
 
+This repo contains the undone pure Haskell XXH3 implementation which only accepts input shorter than 241 bytes. The implementation to handle longer bytes require me to grok on the XXhash C code which is written in SSE/AVX instructions.
+
+Observation from the implementation:
+
+* `fromIntegral` in Haskell is actually quite effecient as it only coerce the numeric type from one to another.
+* The only array-like data structure that is compatible with C is `Data.Array.Storable`. It requies the IO monad, so when dealing with pointer arithematics we have to use `unsafePerformIO`.
+* The XXhash3 is fast because it handle 4 bytes at a time, which is different to FNV-1 and FNV1-a that hash bytes linearly.
+
+**WIP, benchmark results.**
+
+## Comparison
 There are a few haskell xxhash package in hackage, and here is the comparison of them:
 
 - [xxhash-ffi](https://github.com/haskell-haskey/xxhash-ffi) It is using older version of XXHash and connect with GHC FFI. It is the fastest FFI binding because it used a lot [of](https://github.com/haskell-haskey/xxhash-ffi/blob/master/src/Data/Digest/XXHash/FFI.hs#L31-L33) [tricks](https://github.com/haskell-haskey/xxhash-ffi/blob/master/src/Data/Digest/XXHash/FFI.hs#L35-L37) and specializer pragma.
@@ -9,5 +20,3 @@ There are a few haskell xxhash package in hackage, and here is the comparison of
 - [xxhash](https://github.com/christian-marie/xxhash) It is the only xxhash haskell pacakge that is officially [stated](https://code.google.com/archive/p/xxhash/) because it is implemented purely in Haskell!
 
 Both of them are missing the XXHash3 variant which got its API stabled recently.
-
-## This repo contains the undone pure Haskell XXH3 implementation which only accepts input shorter than 241 bytes.
--- bench/Main.hs
+++ bench/Main.hs
@@ -1,26 +1,65 @@
+{-# LANGUAGE MagicHash #-}
 module Main where
 
 import Gauge
 import Control.Monad
 import qualified Data.ByteString.Char8 as B
-
+import Numeric
 import qualified Data.Hashable as H
 import Data.Digest.XXHash
 import System.Random
 import Data.Hash.XXH3
+
+-- import qualified Data.ByteString as B
+import qualified Data.ByteString.Internal as B
+import GHC.ForeignPtr 
+import GHC.Exts (Addr#, Int(I#), plusAddr#, eqAddr#)
+import Data.Word (Word8, Word64)
+import Data.Bits
+import Data.Primitive.Types (indexOffAddr#)
+
+fnv1a64 :: B.ByteString -> Word64
+fnv1a64 (B.PS (ForeignPtr ptr _) (I# offset) (I# len)) = go start end offsetBasis
+  where
+    offsetBasis = 14695981039346656037
+    fnvPrime = 1099511628211
+    start = plusAddr# ptr offset
+    end = plusAddr# start len
+
+    go :: Addr# -> Addr# -> Word64 -> Word64
+    go ptr end hash = case eqAddr# ptr end of
+      1# -> hash
+      _  -> go (plusAddr# ptr 1#) end $ do
+        let byte = indexOffAddr# ptr 0# :: Word8
+        (hash `xor` fromIntegral byte) * fnvPrime
+{-# INLINE fnv1a64 #-}
 
 numbersGen :: IO [B.ByteString]
 numbersGen = do
     xs <- replicateM 33333 (randomRIO (0, 100) :: IO Int)
     pure $ B.pack . show <$> xs
 
+stringsGen :: IO [B.ByteString]
+stringsGen = do
+    xs <- replicateM 33333 (randomRIO (10000, 99999) :: IO Int)
+    pure $ B.pack . (`showHex` "") <$> xs
+
 main :: IO ()
 main = do
     numbers <- numbersGen
+    strings <- stringsGen
     defaultMain [
         bgroup "Numbers" [
-            bench "Data.Hash.XXHash3.FFI" $ whnf (map $ \a -> xxh364 a 0) numbers,
+            bench "Data.Hash.XXH3" $ whnf (map $ \a -> xxh364 a 0) numbers,
             bench "Data.Hashable.hash" $ whnf (map H.hash) numbers,
-            bench "Data.Digest.XXHash Strict" $ whnf (map xxHash') numbers
+            bench "Data.Digest.XXHash Strict" $ whnf (map xxHash') numbers,
+            bench "FNV-1a" $ whnf (map fnv1a64) numbers
+        ],
+        bgroup "Strings" [
+            bench "Data.Hash.XXH3" $ whnf (map $ \a -> xxh364 a 0) strings,
+            bench "Data.Hashable.hash" $ whnf (map H.hash) strings,
+            bench "Data.Digest.XXHash Strict" $ whnf (map xxHash') strings,
+            bench "FNV-1a" $ whnf (map fnv1a64) strings
         ]
+
      ]
